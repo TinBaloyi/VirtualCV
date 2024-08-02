0e039d7edf32f3277325d1f2ccb3f23a301565b5
TinBaloyi <baloyitintswalo92@gmail.com>
2024-08-02 22:14:43 +0200

    done

--- Assets/script.js
+++ Assets/script.js
@@ -50,4 +50,11 @@
               }
             }
           }
+          function toggleMenu() {
+            const menuLinks = document.querySelector('.menu-links');
+            const hamburgerIcon = document.querySelector('.hamburger-icon');
+            menuLinks.classList.toggle('open');
+            hamburgerIcon.classList.toggle('open');
+          }
+          
                 
